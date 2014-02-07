09f3ba59abe714acc03d3011b3070d838f728f45
Aanand Prasad <aanand.prasad@gmail.com>
2014-02-07 11:30:41 -0800

    Extract JSON decoding

--- api/api.go
+++ api/api.go
@@ -28,19 +28,27 @@ func (api HTTPClient) GetAuthToken(username string, password string) (string, er
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-
-	if resp.StatusCode != 200 {
-		return "", errors.New(fmt.Sprintf("erroneous API response: %s", body))
-	}
-
 	var authResponse AuthResponse
-	if err := json.Unmarshal(body, &authResponse); err != nil {
+	if err := DecodeResponse(resp, &authResponse); err != nil {
 		return "", err
 	}
 
 	return authResponse.Token, nil
+}
+
+func DecodeResponse(resp *http.Response, v interface{}) error {
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != 200 {
+		return errors.New(fmt.Sprintf("erroneous API response: %s", body))
+	}
+
+	if err := json.Unmarshal(body, &v); err != nil {
+		return err
+	}
+
+	return nil
 }
