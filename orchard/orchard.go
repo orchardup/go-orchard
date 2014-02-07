86bb02960674188b851d93b5840966432f798fc2
Aanand Prasad <aanand.prasad@gmail.com>
2014-02-07 15:55:32 -0800

    Extract authentication code into method

--- orchard/orchard.go
+++ orchard/orchard.go
@@ -5,22 +5,32 @@ import "github.com/orchardup/orchard/api"
 import "code.google.com/p/gopass"
 
 func main() {
-	var httpClient api.HTTPClient
-	httpClient = api.HTTPClient{"http://localdocker:8000/api/v1", ""}
-
-	username, password := Prompt()
-	token, err := httpClient.GetAuthToken(username, password)
+	httpClient, err := Authenticate()
 	if err != nil {
-		fmt.Printf("Error getting token:\n%s\n", err)
-		return
+		fmt.Printf("Error authenticating:\n%s\n", err)
 	}
-	httpClient.Token = token
+
 	hosts, err := httpClient.GetHosts()
 	if err != nil {
 		fmt.Printf("Error getting hosts:\n%s\n", err)
 		return
 	}
+
 	fmt.Printf("hosts: %v\n", hosts)
+}
+
+func Authenticate() (*api.HTTPClient, error) {
+	httpClient := api.HTTPClient{"http://localdocker:8000/api/v1", ""}
+
+	username, password := Prompt()
+
+	token, err := httpClient.GetAuthToken(username, password)
+	if err != nil {
+		return nil, err
+	}
+	httpClient.Token = token
+
+	return &httpClient, nil
 }
 
 func Prompt() (string, string) {
