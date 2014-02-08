08f7cef7631423e4df5c5fa242ce1b7f1102b160
Aanand Prasad <aanand.prasad@gmail.com>
2014-02-07 17:58:19 -0800

    Load/save token in ~/.orchard/api_tokens

--- orchard/orchard.go
+++ orchard/orchard.go
@@ -1,6 +1,10 @@
 package main
 
 import "fmt"
+import "os"
+import "io"
+import "io/ioutil"
+import "crypto/md5"
 import "github.com/orchardup/orchard/api"
 import "code.google.com/p/gopass"
 
@@ -22,15 +26,63 @@ func main() {
 func Authenticate() (*api.HTTPClient, error) {
 	httpClient := api.HTTPClient{"http://localdocker:8000/api/v1", ""}
 
+	tokenFile, err := GetTokenFilePath(httpClient.BaseURL)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := os.Stat(tokenFile); os.IsNotExist(err) {
+		token, err := GetTokenByPromptingUser(&httpClient)
+		if err != nil {
+			return nil, err
+		}
+		if err := ioutil.WriteFile(tokenFile, []byte(token), 0644); err != nil {
+			return nil, err
+		}
+		httpClient.Token = token
+	} else {
+		token, err := ioutil.ReadFile(tokenFile)
+		if err != nil {
+			return nil, err
+		}
+		httpClient.Token = string(token)
+	}
+
+	return &httpClient, nil
+}
+
+func GetTokenFilePath(baseURL string) (string, error) {
+	tokenDir, err := GetTokenDir()
+	if err != nil {
+		return "", err
+	}
+
+	h := md5.New()
+	io.WriteString(h, baseURL)
+	hash := fmt.Sprintf("%x", h.Sum(nil))
+
+	return tokenDir + hash, nil
+}
+
+func GetTokenDir() (string, error) {
+	tokenDir := os.Getenv("HOME") + "/.orchard/api_tokens/"
+	err := os.MkdirAll(tokenDir, 0700)
+	if err != nil {
+		return "", err
+	}
+	return tokenDir, nil
+}
+
+func GetTokenByPromptingUser(httpClient *api.HTTPClient) (string, error) {
 	username, password := Prompt()
 
 	token, err := httpClient.GetAuthToken(username, password)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 	httpClient.Token = token
 
-	return &httpClient, nil
+	return token, nil
 }
 
 func Prompt() (string, string) {
