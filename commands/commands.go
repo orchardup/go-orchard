d3575cc2ec24d391ac69ba39c3e9c78cecaf2bb0
Aanand Prasad <aanand.prasad@gmail.com>
2014-02-14 12:06:09 -0800

    Use `flag` library to do subcommands and args, like the `go` command

---
+++ commands/commands.go
@@ -0,0 +1,314 @@
+package commands
+
+import (
+	"crypto/tls"
+	"errors"
+	"flag"
+	"fmt"
+	"github.com/orchardup/orchard/authenticator"
+	"github.com/orchardup/orchard/proxy"
+	"github.com/orchardup/orchard/tlsconfig"
+	"github.com/orchardup/orchard/utils"
+	"io/ioutil"
+	"net"
+	"os"
+	"os/exec"
+	"os/signal"
+	"path"
+	"strings"
+	"syscall"
+	"text/tabwriter"
+)
+
+type Command struct {
+	Run       func(cmd *Command, args []string) error
+	UsageLine string
+	Short     string
+	Long      string
+	Flag      flag.FlagSet
+}
+
+func (c *Command) Name() string {
+	name := c.UsageLine
+	i := strings.Index(name, " ")
+	if i >= 0 {
+		name = name[:i]
+	}
+	return name
+}
+
+func (c *Command) Usage() {
+	fmt.Fprintf(os.Stderr, "Usage: %s\n\n", c.UsageLine)
+	fmt.Fprintf(os.Stderr, "%s\n", strings.TrimSpace(c.Long))
+	os.Exit(2)
+}
+
+var All = []*Command{
+	Hosts,
+	Start,
+	Stop,
+	Docker,
+	Proxy,
+}
+
+func init() {
+	Hosts.Run = RunHosts
+	Start.Run = RunStart
+	Stop.Run = RunStop
+	Docker.Run = RunDocker
+	Proxy.Run = RunProxy
+}
+
+var Hosts = &Command{
+	UsageLine: "hosts",
+	Short:     "List all hosts",
+	Long:      "List all hosts",
+}
+
+var Start = &Command{
+	UsageLine: "start [NAME]",
+	Short:     "Start a host",
+	Long: `Start a host.
+
+You can optionally specify a name for the host - if not, it will be
+named 'default', and 'orchard docker' commands will use it automatically.`,
+}
+
+var Stop = &Command{
+	UsageLine: "stop [NAME]",
+	Short:     "Stop a host",
+	Long: `Stop a host.
+
+You can optionally specify which host to stop - if you don't, the default
+host (named 'default') will be stopped.`,
+}
+
+var Docker = &Command{
+	UsageLine: "docker [-H HOST] [COMMAND...]",
+	Short:     "Run a Docker command against a host",
+	Long: `Run a Docker command against a host.
+
+Wraps the 'docker' command-line tool - see the Docker website for reference:
+
+    http://docs.docker.io/en/latest/reference/commandline/
+
+You can optionally specify a host by name - if you don't, the default host
+will be used.`,
+}
+
+var flDockerHost = Docker.Flag.String("H", "", "")
+
+var Proxy = &Command{
+	UsageLine: "proxy [-H HOST]",
+	Short:     "Start a local proxy to a host's Docker daemon",
+	Long: `Start a local proxy to a host's Docker daemon.
+
+Prints out a URL to pass to the 'docker' command, e.g.
+
+    $ orchard proxy
+    Started proxy at unix:///tmp/orchard-12345/orchard.sock
+
+    $ docker -H unix:///tmp/orchard-12345/orchard.sock run ubuntu echo hello world
+    hello world
+`,
+}
+
+var flProxyHost = Proxy.Flag.String("H", "", "")
+
+func RunHosts(cmd *Command, args []string) error {
+	httpClient, err := authenticator.Authenticate()
+	if err != nil {
+		return err
+	}
+
+	hosts, err := httpClient.GetHosts()
+	if err != nil {
+		return err
+	}
+
+	writer := tabwriter.NewWriter(os.Stdout, 20, 1, 3, ' ', 0)
+	fmt.Fprintln(writer, "NAME\tIP")
+	for _, host := range hosts {
+		fmt.Fprintf(writer, "%s\t%s\n", host.Name, host.IPAddress)
+	}
+	writer.Flush()
+
+	return nil
+}
+
+func RunStart(cmd *Command, args []string) error {
+	httpClient, err := authenticator.Authenticate()
+	if err != nil {
+		return err
+	}
+
+	hostName, humanName := GetHostName(args)
+	humanName = utils.Capitalize(humanName)
+
+	host, err := httpClient.CreateHost(hostName)
+	if err != nil {
+		// HACK. api.go should decode JSON and return a specific type of error for this case.
+		if strings.Contains(err.Error(), "already exists") {
+			fmt.Fprintf(os.Stderr, "%s is already running.\nYou can create additional hosts with `orchard start NAME`.\n", humanName)
+			return nil
+		}
+		if strings.Contains(err.Error(), "Invalid value") {
+			fmt.Fprintf(os.Stderr, "Sorry, '%s' isn't a valid host name.\nHost names can only contain lowercase letters, numbers and underscores.\n", hostName)
+			return nil
+		}
+
+		return err
+	}
+	fmt.Fprintf(os.Stderr, "%s running at %s\n", humanName, host.IPAddress)
+
+	return nil
+}
+
+func RunStop(cmd *Command, args []string) error {
+	hostName, humanName := GetHostName(args)
+
+	var confirm string
+	fmt.Printf("Going to stop and delete %s. All data on it will be lost.\n", humanName)
+	fmt.Print("Are you sure you're ready? [yN] ")
+	fmt.Scanln(&confirm)
+
+	if strings.ToLower(confirm) != "y" {
+		return nil
+	}
+
+	httpClient, err := authenticator.Authenticate()
+	if err != nil {
+		return err
+	}
+
+	err = httpClient.DeleteHost(hostName)
+	if err != nil {
+		// HACK. api.go should decode JSON and return a specific type of error for this case.
+		if strings.Contains(err.Error(), "Not found") {
+			fmt.Fprintf(os.Stderr, "%s doesn't seem to be running.\nYou can view your running hosts with `orchard hosts`.\n", utils.Capitalize(humanName))
+			return nil
+		}
+
+		return err
+	}
+	fmt.Fprintf(os.Stderr, "Stopped %s\n", humanName)
+
+	return nil
+}
+
+func RunDocker(cmd *Command, args []string) error {
+	return WithDockerProxy(func(socketPath string) error {
+		err := CallDocker(args, []string{"DOCKER_HOST=unix://" + socketPath})
+		if err != nil {
+			return fmt.Errorf("Docker exited with error")
+		}
+		return nil
+	})
+}
+
+func RunProxy(cmd *Command, args []string) error {
+	return WithDockerProxy(func(socketPath string) error {
+		fmt.Fprintln(os.Stderr, "Started proxy at unix://"+socketPath)
+
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGKILL)
+		<-c
+
+		fmt.Fprintln(os.Stderr, "\nStopping proxy")
+		return nil
+	})
+}
+
+func WithDockerProxy(callback func(string) error) error {
+	hostName := "default"
+	if *flDockerHost != "" {
+		hostName = *flDockerHost
+	}
+
+	dirname, err := ioutil.TempDir("/tmp", "orchard-")
+	if err != nil {
+		return fmt.Errorf("Error creating temporary directory: %s\n", err)
+	}
+	defer os.RemoveAll(dirname)
+	socketPath := path.Join(dirname, "orchard.sock")
+
+	p, err := MakeProxy(socketPath, hostName)
+	if err != nil {
+		return fmt.Errorf("Error starting proxy: %v\n", err)
+	}
+
+	go p.Start()
+	defer p.Stop()
+
+	if err := <-p.ErrorChannel; err != nil {
+		return fmt.Errorf("Error starting proxy: %v\n", err)
+	}
+
+	if err := callback(socketPath); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func MakeProxy(socketPath string, hostName string) (*proxy.Proxy, error) {
+	httpClient, err := authenticator.Authenticate()
+	if err != nil {
+		return nil, err
+	}
+
+	host, err := httpClient.GetHost(hostName)
+	if err != nil {
+		return nil, err
+	}
+	destination := host.IPAddress + ":4243"
+
+	certData := []byte(host.ClientCert)
+	keyData := []byte(host.ClientKey)
+	config, err := tlsconfig.GetTLSConfig(certData, keyData)
+	if err != nil {
+		return nil, err
+	}
+
+	return proxy.New(
+		func() (net.Listener, error) { return net.Listen("unix", socketPath) },
+		func() (net.Conn, error) { return tls.Dial("tcp", destination, config) },
+	), nil
+}
+
+func CallDocker(args []string, env []string) error {
+	dockerPath := GetDockerPath()
+	if dockerPath == "" {
+		return errors.New("Can't find `docker` executable in $PATH.\nYou might need to install it: http://docs.docker.io/en/latest/installation/#installation-list")
+	}
+
+	cmd := exec.Command(dockerPath, args...)
+	cmd.Env = env
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func GetDockerPath() string {
+	for _, dir := range strings.Split(os.Getenv("PATH"), ":") {
+		dockerPath := path.Join(dir, "docker")
+		_, err := os.Stat(dockerPath)
+		if err == nil {
+			return dockerPath
+		}
+	}
+	return ""
+}
+
+func GetHostName(args []string) (string, string) {
+	hostName := "default"
+	humanName := "default host"
+
+	if len(args) > 0 {
+		hostName = args[0]
+		humanName = fmt.Sprintf("host '%s'", hostName)
+	}
+
+	return hostName, humanName
+}
--- main.go
+++ main.go
@@ -1,246 +1,96 @@
 package main
 
 import (
-	"crypto/tls"
-	"errors"
+	"flag"
 	"fmt"
-	"github.com/orchardup/orchard/cli"
-	"github.com/orchardup/orchard/proxy"
-	"github.com/orchardup/orchard/tlsconfig"
-	"github.com/orchardup/orchard/utils"
-	"github.com/orchardup/orchard/vendor/github.com/docopt/docopt.go"
-	"io/ioutil"
-	"net"
+	"github.com/orchardup/orchard/commands"
+	"io"
 	"os"
-	"os/exec"
-	"os/signal"
-	"path"
 	"strings"
-	"syscall"
-	"text/tabwriter"
+	"text/template"
 )
 
 func main() {
-	usage := `Orchard.
+	flag.Usage = usage
+	flag.Parse()
 
-Usage:
-  orchard hosts
-  orchard start [NAME]
-  orchard stop [NAME]
-  orchard [options] docker [COMMAND...]
-  orchard [options] proxy
+	args := flag.Args()
+	if len(args) < 1 {
+		usage()
+	}
 
-Options:
-  -h --help               Show this screen.
-  --version               Show version.
-  -H NAME, --host=NAME    Name of host to connect to (instead of 'default')`
-
-	args, err := docopt.Parse(usage, nil, true, "Orchard 2.0.0", true)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error parsing arguments: %s\n", err)
+	if args[0] == "help" {
+		help(args[1:])
 		return
 	}
 
-	var cmdErr error = nil
-
-	if args["hosts"] == true {
-		cmdErr = Hosts(args)
-	} else if args["start"] == true {
-		cmdErr = Start(args)
-	} else if args["stop"] == true {
-		cmdErr = Stop(args)
-	} else if args["docker"] == true || args["proxy"] == true {
-		cmdErr = Docker(args)
-	}
-
-	if cmdErr != nil {
-		fmt.Fprintln(os.Stderr, cmdErr)
-		os.Exit(1)
-	}
-}
-
-func Docker(args map[string]interface{}) error {
-	hostName := "default"
-	if args["--host"] != nil {
-		hostName = args["--host"].(string)
-	}
-
-	dirname, err := ioutil.TempDir("/tmp", "orchard-")
-	if err != nil {
-		return fmt.Errorf("Error creating temporary directory: %s\n", err)
-	}
-	defer os.RemoveAll(dirname)
-	socketPath := path.Join(dirname, "orchard.sock")
-
-	p, err := MakeProxy(socketPath, hostName)
-	if err != nil {
-		return fmt.Errorf("Error starting proxy: %v\n", err)
-	}
-
-	go p.Start()
-	defer p.Stop()
-
-	if err := <-p.ErrorChannel; err != nil {
-		return fmt.Errorf("Error starting proxy: %v\n", err)
-	}
-
-	if args["docker"] == true {
-		err := CallDocker(args["COMMAND"].([]string), []string{"DOCKER_HOST=unix://" + socketPath})
-		if err != nil {
-			return fmt.Errorf("Docker exited with error")
-		}
-	} else {
-		fmt.Fprintln(os.Stderr, "Started proxy at unix://"+socketPath)
-
-		c := make(chan os.Signal)
-		signal.Notify(c, syscall.SIGINT, syscall.SIGKILL)
-		<-c
-
-		fmt.Fprintln(os.Stderr, "\nStopping proxy")
-	}
-
-	return nil
-}
-
-func MakeProxy(socketPath string, hostName string) (*proxy.Proxy, error) {
-	httpClient, err := authenticator.Authenticate()
-	if err != nil {
-		return nil, err
-	}
-
-	host, err := httpClient.GetHost(hostName)
-	if err != nil {
-		return nil, err
-	}
-	destination := host.IPAddress + ":4243"
-
-	certData := []byte(host.ClientCert)
-	keyData := []byte(host.ClientKey)
-	config, err := tlsconfig.GetTLSConfig(certData, keyData)
-	if err != nil {
-		return nil, err
-	}
-
-	return proxy.New(
-		func() (net.Listener, error) { return net.Listen("unix", socketPath) },
-		func() (net.Conn, error) { return tls.Dial("tcp", destination, config) },
-	), nil
-}
-
-func CallDocker(args []string, env []string) error {
-	dockerPath := GetDockerPath()
-	if dockerPath == "" {
-		return errors.New("Can't find `docker` executable in $PATH.\nYou might need to install it: http://docs.docker.io/en/latest/installation/#installation-list")
-	}
-
-	cmd := exec.Command(dockerPath, args...)
-	cmd.Env = env
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	return cmd.Run()
-}
-
-func GetDockerPath() string {
-	for _, dir := range strings.Split(os.Getenv("PATH"), ":") {
-		dockerPath := path.Join(dir, "docker")
-		_, err := os.Stat(dockerPath)
-		if err == nil {
-			return dockerPath
+	for _, cmd := range commands.All {
+		if cmd.Name() == args[0] {
+			cmd.Flag.Usage = func() { cmd.Usage() }
+			cmd.Flag.Parse(args[1:])
+			args = cmd.Flag.Args()
+			err := cmd.Run(cmd, args)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+			return
 		}
 	}
-	return ""
+
+	fmt.Fprintf(os.Stderr, "Unknown subcommand: %q\n\n", args[0])
+	usage()
 }
 
-func Start(args map[string]interface{}) error {
-	httpClient, err := authenticator.Authenticate()
-	if err != nil {
-		return err
+var usageTemplate = `Orchard command-line client.
+
+Usage: orchard COMMAND [ARG...]
+
+Commands:
+{{range .}}
+  {{.Name | printf "%-11s"}} {{.Short}}{{end}}
+
+Run 'orchard help command' for more information on a command.`
+
+var helpTemplate = `Usage: orchard {{.UsageLine}}
+
+{{.Long | trim}}
+`
+
+func tmpl(w io.Writer, text string, data interface{}) {
+	t := template.New("top")
+	t.Funcs(template.FuncMap{"trim": strings.TrimSpace})
+	template.Must(t.Parse(text))
+	if err := t.Execute(w, data); err != nil {
+		panic(err)
+	}
+}
+
+func printUsage(w io.Writer) {
+	tmpl(w, usageTemplate, commands.All)
+}
+
+func usage() {
+	printUsage(os.Stderr)
+	os.Exit(2)
+}
+
+func help(args []string) {
+	if len(args) == 0 {
+		printUsage(os.Stdout)
+		return
 	}
 
-	hostName, humanName := GetHostName(args)
-	humanName = utils.Capitalize(humanName)
+	arg := args[0]
 
-	host, err := httpClient.CreateHost(hostName)
-	if err != nil {
-		// HACK. api.go should decode JSON and return a specific type of error for this case.
-		if strings.Contains(err.Error(), "already exists") {
-			fmt.Fprintf(os.Stderr, "%s is already running.\nYou can create additional hosts with `orchard start NAME`.\n", humanName)
-			return nil
+	for _, cmd := range commands.All {
+		if cmd.Name() == arg {
+			tmpl(os.Stdout, helpTemplate, cmd)
+			return
 		}
-		if strings.Contains(err.Error(), "Invalid value") {
-			fmt.Fprintf(os.Stderr, "Sorry, '%s' isn't a valid host name.\nHost names can only contain lowercase letters, numbers and underscores.\n", hostName)
-			return nil
-		}
-
-		return err
-	}
-	fmt.Fprintf(os.Stderr, "%s running at %s\n", humanName, host.IPAddress)
-
-	return nil
-}
-
-func Stop(args map[string]interface{}) error {
-	hostName, humanName := GetHostName(args)
-
-	var confirm string
-	fmt.Printf("Going to stop and delete %s. All data on it will be lost.\n", humanName)
-	fmt.Print("Are you sure you're ready? [yN] ")
-	fmt.Scanln(&confirm)
-
-	if strings.ToLower(confirm) != "y" {
-		return nil
 	}
 
-	httpClient, err := authenticator.Authenticate()
-	if err != nil {
-		return err
-	}
-
-	err = httpClient.DeleteHost(hostName)
-	if err != nil {
-		// HACK. api.go should decode JSON and return a specific type of error for this case.
-		if strings.Contains(err.Error(), "Not found") {
-			fmt.Fprintf(os.Stderr, "%s doesn't seem to be running.\nYou can view your running hosts with `orchard hosts`.\n", utils.Capitalize(humanName))
-			return nil
-		}
-
-		return err
-	}
-	fmt.Fprintf(os.Stderr, "Stopped %s\n", humanName)
-
-	return nil
-}
-
-func GetHostName(args map[string]interface{}) (string, string) {
-	hostName := "default"
-	humanName := "default host"
-
-	if args["NAME"] != nil {
-		hostName = args["NAME"].(string)
-		humanName = fmt.Sprintf("host '%s'", hostName)
-	}
-
-	return hostName, humanName
-}
-
-func Hosts(args map[string]interface{}) error {
-	httpClient, err := authenticator.Authenticate()
-	if err != nil {
-		return err
-	}
-
-	hosts, err := httpClient.GetHosts()
-	if err != nil {
-		return err
-	}
-
-	writer := tabwriter.NewWriter(os.Stdout, 20, 1, 3, ' ', 0)
-	fmt.Fprintln(writer, "NAME\tIP")
-	for _, host := range hosts {
-		fmt.Fprintf(writer, "%s\t%s\n", host.Name, host.IPAddress)
-	}
-	writer.Flush()
-
-	return nil
+	fmt.Fprintf(os.Stderr, "Unknown help topic %#q.\n\n", arg)
+	printUsage(os.Stderr)
+	os.Exit(2)
 }
